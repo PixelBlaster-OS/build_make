73aa3184fb0fdd15935e3649b4fd7a9cd32f0657
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-04-29 10:28:59 +0530

    Build: PixelBlaster

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -271,8 +271,8 @@ class EdifyGenerator(object):
       self.Print("        PixelExperience (Plus edition)")
       self.Print("                by jhenrique09")
     else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+      self.Print("              PixelBlaster-OS")
+      self.Print("              by TheTablaster & KKShedge")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
     self.Print(" Build id: %s"%(build_id))
